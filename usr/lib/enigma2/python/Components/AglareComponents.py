61a9c350b20e0ff0ab900b12d1c550f8f2c5df93
nonsolosat <nonsolosat@nonsolosat.homenet.telecomitalia.it>
2024-11-15 19:11:55 +0100

    update

--- usr/lib/enigma2/python/Components/AglareComponents.py
+++ usr/lib/enigma2/python/Components/AglareComponents.py
@@ -1,19 +1,25 @@
-#import inspect
-from __future__ import absolute_import #zmiana strategii ladowanie modulow w py2 z relative na absolute jak w py3
-from os import path, system
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import absolute_import
 from datetime import datetime
-import re, unicodedata
+from os import path
+import re
+from six import text_type
+import unicodedata
 
-append2file=False
-imageType=None
 
+append2file = False
+imageType = None
 PYversion = None
 
-def WhatPythonVersion():
-  import sys
-  return sys.version_info[0]
 
-def isPY2(): #from Components.j00zekComponents import isPY2 then use if isPY2()
+def WhatPythonVersion():
+    import sys
+    return sys.version_info[0]
+
+
+def isPY2():
     global PYversion
     if PYversion is None:
         if WhatPythonVersion() == 3:
@@ -22,12 +28,13 @@ def isPY2(): #from Components.j00zekComponents import isPY2 then use if isPY2()
             PYversion = True
     return PYversion
 
-def ensure_binary(text, encoding='utf-8', errors='strict'): #based on six library
+
+def ensure_binary(text, encoding='utf-8', errors='strict'):
     if isPY2():
         return text
-    else: #PY3
+    else:
         if isinstance(text, bytes):
-          return text
+            return text
         if isinstance(text, str):
             try:
                 return text.encode(encoding, errors)
@@ -35,7 +42,25 @@ def ensure_binary(text, encoding='utf-8', errors='strict'): #based on six librar
                 return text.encode(encoding, 'ignore')
     return text
 
-def ensure_str(text, encoding='utf-8', errors='strict'): #from Components.j00zekComponents import ensure_str
+
+def ensure_str(text, encoding='utf-8', errors='strict'):
+    if isinstance(text, str):
+        return text
+    if isinstance(text, text_type):  # Compatibilità per Python 2 e 3
+        try:
+            return text.encode(encoding, errors)
+        except Exception:
+            return text.encode(encoding, 'ignore')
+    elif isinstance(text, bytes):
+        try:
+            return text.decode(encoding, errors)
+        except Exception:
+            return text.decode(encoding, 'ignore')
+    return text
+
+
+'''
+def ensure_str(text, encoding='utf-8', errors='strict'):
     if type(text) is str:
         return text
     if isPY2():
@@ -44,23 +69,27 @@ def ensure_str(text, encoding='utf-8', errors='strict'): #from Components.j00zek
                 return text.encode(encoding, errors)
             except Exception:
                 return text.encode(encoding, 'ignore')
-    else: #PY3
+    else:
         if isinstance(text, bytes):
             try:
                 return text.decode(encoding, errors)
             except Exception:
                 return text.decode(encoding, 'ignore')
-    return text # strwithmeta type defined in e2iplayer goes thorugh it
-  
+    return text
+'''
+
+
 def clearCache():
-    with open("/proc/sys/vm/drop_caches", "w") as f: f.write("1\n")
+    with open("/proc/sys/vm/drop_caches", "w") as f:
+        f.write("1\n")
+
 
 def getImageType():
     return imageType
 
-def isImageType(imgName = ''):
+
+def isImageType(imgName=''):
     global imageType
-    #check using opkg
     if imageType is None:
         if path.exists('/etc/opkg/all-feed.conf'):
             with open('/etc/opkg/all-feed.conf', 'r') as file:
@@ -77,7 +106,6 @@ def isImageType(imgName = ''):
                     imageType = 'openatv'
                     if fileContent.find('/5.3/') > -1:
                         imageType += '5.3'
-    #check using specifics
     if imageType is None:
         if path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/VTIPanel/'):
             imageType = 'vti'
@@ -89,100 +117,152 @@ def isImageType(imgName = ''):
             imageType = 'pkt'
         else:
             imageType = 'unknown'
-    if imgName.lower() == imageType.lower() :
+    if imgName.lower() == imageType.lower():
         return True
     else:
         return False
 
-def AGDEBUG(myText = None, Append = True, myDEBUG = '/tmp/AglareComponents.log'):
+
+def AGDEBUG(myText=None, Append=True, myDEBUG='/tmp/AglareComponents.log'):
     global append2file
-    if myDEBUG is None:
-        return
-    if myText is None:
+    if myDEBUG is None or myText is None:
         return
     try:
-        if append2file == False or Append == False:
+        if append2file is False or Append is False:
+            append2file = True
+            mode = 'w'
+        else:
+            mode = 'a'
+        with open(myDEBUG, mode) as f:
+            f.write('%s\t%s\n' % (str(datetime.now()), myText))
+        if path.getsize(myDEBUG) > 100000:
+            with open(myDEBUG, 'r+') as f:
+                lines = f.readlines()
+                f.seek(0)
+                f.writelines(lines[10:])
+                f.truncate()
+    except Exception as e:
+        with open(myDEBUG, 'a') as f:
+            f.write('Exception: %s\n' % str(e))
+    return
+
+
+def logMissing(myText=None, Append=True, myDEBUG='/tmp/AglareComponents.log'):
+    global append2file
+
+    if myDEBUG is None or myText is None:
+        return
+    try:
+        if append2file is False or Append is False:
             append2file = True
             f = open(myDEBUG, 'w')
         else:
             f = open(myDEBUG, 'a')
-        f.write('%s\t%s\n' % (str(datetime.now()),myText))
+        f.write('%s\t%s\n' % (str(datetime.now()), myText))
         f.close()
+        # Check if the file exceeds 100 KB
         if path.getsize(myDEBUG) > 100000:
-            system('sed -i -e 1,10d %s' % myDEBUG)
-        #print(myText)
+            with open(myDEBUG, 'r+') as f:
+                lines = f.readlines()
+                f.seek(0)
+                f.writelines(lines[10:])  # Skip the first 10 lines
+                f.truncate()
     except Exception as e:
-        system('echo "Exception:%s" >> %s' %( str(e), myDEBUG ))
+        print('Exception:', e)
+
     return
 
-def logMissing(myText = None, Append = True, myDEBUG = '/tmp/AglareComponents.log'):
-    global append2file
-    if myDEBUG is None:
-        return
-    if myText is None:
-        return
-    try:
-        if append2file == False or Append == False:
-            append2file = True
-            f = open(myDEBUG, 'w')
-        else:
-            f = open(myDEBUG, 'a')
-        f.write('%s\t%s\n' % (str(datetime.now()),myText))
-        f.close()
-        if path.getsize(myDEBUG) > 100000:
-            system('sed -i -e 1,10d %s' % myDEBUG)
-        #print(myText)
-    except Exception as e:
-        pass
-    return
 
-def isINETworking(addr = '8.8.8.8', port = 53):
+'''
+# def AGDEBUG(myText=None, Append=True, myDEBUG='/tmp/AglareComponents.log'):
+    # global append2file
+    # if myDEBUG is None:
+        # return
+    # if myText is None:
+        # return
+    # try:
+        # if append2file is False or Append is False:
+            # append2file = True
+            # f = open(myDEBUG, 'w')
+        # else:
+            # f = open(myDEBUG, 'a')
+        # f.write('%s\t%s\n' % (str(datetime.now()), myText))
+        # f.close()
+        # if path.getsize(myDEBUG) > 100000:
+            # system('sed -i -e 1,10d %s' % myDEBUG)
+    # except Exception as e:
+        # system('echo "Exception:%s" >> %s' % (str(e), myDEBUG))
+    # return
+
+
+# def logMissing(myText=None, Append=True, myDEBUG='/tmp/AglareComponents.log'):
+    # global append2file
+    # if myDEBUG is None:
+        # return
+    # if myText is None:
+        # return
+    # try:
+        # if append2file is False or Append is False:
+            # append2file = True
+            # f = open(myDEBUG, 'w')
+        # else:
+            # f = open(myDEBUG, 'a')
+        # f.write('%s\t%s\n' % (str(datetime.now()), myText))
+        # f.close()
+        # if path.getsize(myDEBUG) > 100000:
+            # system('sed -i -e 1,10d %s' % myDEBUG)
+    # except Exception as e:
+        # print(e)
+        # pass
+    # return
+'''
+
+
+def isINETworking(addr='8.8.8.8', port=53):
     try:
         import socket
-        if addr[:1].isdigit(): addr = socket.gethostbyname(addr)
+        if addr[:1].isdigit():
+            addr = socket.gethostbyname(addr)
         socket.setdefaulttimeout(0.5)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((addr, port))#connection with google dns service
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((addr, port))
         return True
     except Exception as e:
+        print(e)
         pass
-        #printDEBUG("%s" % str(e))
-    #printDEBUG("Error no internet connection. > %s" % str(e))
     return False
-  
-def CHname_2_piconName(serName, iptvStream = False):
+
+
+def CHname_2_piconName(serName, iptvStream=False):
     piconName = serName.lower()
     if iptvStream:
-        piconName = piconName.replace(' fhd', ' hd').replace(' uhd', ' hd') #iptv streams names correction
-    piconName = unicodedata.normalize('NFKD', unicode(piconName, 'utf_8', errors='ignore')).encode('ASCII', 'ignore')
+        piconName = piconName.replace(' fhd', ' hd').replace(' uhd', ' hd')
+    piconName = unicodedata.normalize('NFKD', text_type(piconName, 'utf_8', errors='ignore')).encode('ASCII', 'ignore')
     piconName = re.sub('[^a-z0-9]', '', piconName.replace('&', 'and').replace('+', 'plus').replace('*', 'star'))
     return piconName
 
-if isPY2():
+
+try:
+    # Python 2 imports
     from urlparse import urljoin, urlparse, urlunparse, urlsplit, urlunsplit, parse_qs, parse_qsl
-    from urllib2 import BaseHandler          as urllib2_BaseHandler,         build_opener        as urllib2_build_opener, \
-                        HTTPCookieProcessor   as urllib2_HTTPCookieProcessor, HTTPError           as urllib2_HTTPError, \
-                        HTTPHandler           as urllib2_HTTPHandler,         HTTPRedirectHandler as urllib2_HTTPRedirectHandler, \
-                        HTTPSHandler          as urllib2_HTTPSHandler,        ProxyHandler        as urllib2_ProxyHandler, \
-                        Request               as urllib2_Request,             URLError            as urllib2_URLError, \
-                        urlopen               as urllib2_urlopen,             install_opener      as urllib2_install_opener
-                        
-    from urllib import addinfourl             as urllib_addinfourl,           quote               as urllib_quote, \
-                       quote_plus             as urllib_quote_plus,           unquote             as urllib_unquote, \
-                       unquote_plus           as urllib_unquote_plus,         urlencode           as urllib_urlencode, \
-                       urlopen                as urllib_urlopen,              urlretrieve         as urllib_urlretrieve
-else:
+    from urllib import addinfourl as urllib_addinfourl, quote as urllib_quote, quote_plus as urllib_quote_plus
+    from urllib import unquote as urllib_unquote, unquote_plus as urllib_unquote_plus, urlencode as urllib_urlencode
+    from urllib import urlopen as urllib_urlopen, urlretrieve as urllib_urlretrieve
+    from urllib2 import BaseHandler as urllib2_BaseHandler, build_opener as urllib2_build_opener
+    from urllib2 import HTTPCookieProcessor as urllib2_HTTPCookieProcessor, HTTPError as urllib2_HTTPError
+    from urllib2 import HTTPHandler as urllib2_HTTPHandler, HTTPRedirectHandler as urllib2_HTTPRedirectHandler
+    from urllib2 import HTTPSHandler as urllib2_HTTPSHandler, ProxyHandler as urllib2_ProxyHandler
+    from urllib2 import Request as urllib2_Request, URLError as urllib2_URLError, urlopen as urllib2_urlopen
+    from urllib2 import install_opener as urllib2_install_opener
+
+except ImportError:
+    # Python 3 imports
     from urllib.parse import urljoin, urlparse, urlunparse, urlsplit, urlunsplit, parse_qs, parse_qsl
-    from urllib.request import addinfourl     as urllib_addinfourl,           BaseHandler         as urllib2_BaseHandler, \
-                               build_opener   as urllib2_build_opener,        HTTPCookieProcessor as urllib2_HTTPCookieProcessor, \
-                               HTTPHandler    as urllib2_HTTPHandler,         HTTPRedirectHandler as urllib2_HTTPRedirectHandler, \
-                               HTTPSHandler   as urllib2_HTTPSHandler,        ProxyHandler        as urllib2_ProxyHandler, \
-                               Request        as urllib2_Request,             urlopen             as urllib2_urlopen, \
-                               urlopen        as urllib_urlopen,              urlretrieve         as urllib_urlretrieve, \
-                               install_opener as urllib2_install_opener
-    
-    from urllib.parse import quote            as urllib_quote,                quote_plus          as urllib_quote_plus, \
-                             unquote          as urllib_unquote,              unquote_plus        as urllib_unquote_plus, \
-                             urlencode           as urllib_urlencode
-    
-    from urllib.error import HTTPError        as urllib2_HTTPError,           URLError            as urllib2_URLError
-    
+    from urllib.parse import quote as urllib_quote, quote_plus as urllib_quote_plus
+    from urllib.parse import unquote as urllib_unquote, unquote_plus as urllib_unquote_plus, urlencode as urllib_urlencode
+    from urllib.request import addinfourl as urllib_addinfourl, build_opener as urllib2_build_opener
+    from urllib.request import BaseHandler as urllib2_BaseHandler, HTTPCookieProcessor as urllib2_HTTPCookieProcessor
+    from urllib.request import HTTPHandler as urllib2_HTTPHandler, HTTPRedirectHandler as urllib2_HTTPRedirectHandler
+    from urllib.request import HTTPSHandler as urllib2_HTTPSHandler, ProxyHandler as urllib2_ProxyHandler
+    from urllib.request import Request as urllib2_Request, urlopen as urllib2_urlopen, urlopen as urllib_urlopen
+    from urllib.request import urlretrieve as urllib_urlretrieve, install_opener as urllib2_install_opener
+    from urllib.error import HTTPError as urllib2_HTTPError, URLError as urllib2_URLError
