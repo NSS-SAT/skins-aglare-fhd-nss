b7a93cc678dd693f16a51fea793891e323f26872
NEDVED <81510045+NSS-SAT@users.noreply.github.com>
2024-11-26 19:44:18 +0100

    Update AglarePosterX.py

--- usr/lib/enigma2/python/Components/Renderer/AglarePosterX.py
+++ usr/lib/enigma2/python/Components/Renderer/AglarePosterX.py
@@ -182,13 +182,13 @@ else:
                     apdb[i] = service
 
 
-try:
-    folder_size = sum([sum(map(lambda fname: os.path.getsize(os.path.join(path_folder, fname)), files)) for folder_p, folders, files in os.walk(path_folder)])
-    agposter = "%0.f" % (folder_size / (1024 * 1024.0))
-    if agposter >= "5":
-        shutil.rmtree(path_folder)
-except:
-    pass
+# try:
+    # folder_size = sum([sum(map(lambda fname: os.path.getsize(os.path.join(path_folder, fname)), files)) for folder_p, folders, files in os.walk(path_folder)])
+    # agposter = "%0.f" % (folder_size / (1024 * 1024.0))
+    # if agposter >= "5":
+        # shutil.rmtree(path_folder)
+# except:
+    # pass
 
 
 def OnclearMem():
